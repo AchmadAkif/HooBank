f34bdf4399f93234cd05977df01a562d5f5ebd52
achmadakif <achmadakif7@gmail.com>
2023-09-25 19:15:04 +0700

    stats section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,20 +6,20 @@ import { robot, discount } from '../assets/index'
 const Hero = () => (
   <section id='home' className={`flex md:flex-row flex-col ${style.paddingY} font-poppins`}>
     <div className={`flex-1 ${style.flexStart} flex-col xl:px-0 sm:px-16 px-6`}>
-      <div className='flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2'>
+      <div className='flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] ss:mb-2 mb-7'>
         <img src={discount} />
         <p className={`${style.paragraph} ml-[13px]`}><span className='text-white'>20% </span>DISCOUNT FOR <span className='text-white'>1 MONTH </span>ACCOUNT</p>
       </div>
 
-      <div className='flex ss:flex-row flex-col items-center w-full mb-5'>
-        <h1 className='flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] ss:leading-[100px] text-white mb-5'>The Next<br /><span className='text-gradient'>Generation</span><br />Payment Method.</h1>
+      <div className='flex ss:flex-row flex-col items-center w-full ss:mb-5 mb-14'>
+        <h1 className='flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] ss:leading-[100px] text-white mb-3'>The Next<br /><span className='text-gradient'>Generation</span><br />Payment Method.</h1>
         <GetStarted />
       </div>
 
-      <p className={`${style.paragraph}`}>Our team experts uses a methodology to identify <br className />the credit cards most likely to fit your needs. <br className />We examine annual percentage rates, annual fees.</p>
+      <p className={`${style.paragraph}`}>Our team experts uses a methodology to identify the credit cards most likely to fit your needs. We examine annual percentage rates, annual fees.</p>
 
     </div>
-    <img src={robot} alt="robot-hand" className='w-[669px]' />
+    <img src={robot} alt="robot-hand" className='w-[669px] ss:flex hidden' />
   </section>
 )
 
--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import style from '../style'
+import { stats } from '../static/text'
 
-const Stats = () => {
+const Stats = () => {  
   return (
-    <div>Stats</div>
+    <section className={`flex md:flex-row flex-col ${style.paddingY} font-poppins`}>
+      <div className='flex ss:flex-row flex-col flex-1 justify-between items-center px-6 ss:gap-0 gap-[50px]'>
+        {stats.map( e => (
+          <div className='flex ss:flex-row flex-col font-poppins items-center ss:gap-[25px] gap-0'>
+            <p className='text-white text-[40px] font-semibold'>{e.value}</p>
+            <p className='text-gradient text-[20px]'>{e.title}</p>
+          </div>
+        ))}
+      </div>
+    </section>
   )
 }
 
