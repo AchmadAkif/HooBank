612b8ba876f90d9f4ae20f47b2cd7a060528bb26
achmadakif <achmadakif7@gmail.com>
2023-09-25 17:18:17 +0700

    adjust config and hero section

--- src/components/GetStarted.jsx
+++ src/components/GetStarted.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import style from '../style'
+import { arrowUp } from '../assets'
 
 const GetStarted = () => {
   return (
-    <div>GetStarted</div>
+    <div className={`${style.flexCenter} ss:w-[140px] w-full ss:h-[140px] ss:rounded-full rounded-full border-2 border-immerBlue cursor-pointer text-gradient`}>
+      <div>
+        <p className='flex gap-[5px]'>Get <img src={arrowUp}/></p>
+        <p>Started</p>
+      </div>
+    </div>
   )
 }
 
