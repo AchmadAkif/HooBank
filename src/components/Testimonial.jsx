2ce76d929c530b61b1e3e42d418420fa6a865e36
achmadakif <achmadakif7@gmail.com>
2023-09-28 20:20:38 +0700

    TestimonialCard adjustment

--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -5,14 +5,14 @@ import { quotes } from '../assets'
 
 const TestimonialCard = ({content, title, name, img}) => {
   return (
-    <div className='w-[370px] h-[395px] feature-card py-[60px] px-[40px] rounded-[20px]'>
-      <img src={quotes} />
-      <p className='text-dimWhite text-[18px]'>{content}</p>
-      <div className='flex flex-1 space-x-5 items-center'>
-        <img src={img} className='w-[48px]' />
-        <div>
-          <p className='text-white text-[20px]'>{name}</p>
-          <p className='text-dimWhite text-[16px]'>{title}</p>
+    <div className='flex flex-col justify-between px-10 py-12 rounded-[20px] max-w-[370px] md:mr-10 sm:mr-5 mr-0 my-5 feedback-card'>
+      <img src={quotes} className='w-[42px] h-[27px] object-contain'/>
+      <p className='font-poppins font-normal text-[18px] text-white leading-[32px] my-10'>{content}</p>
+      <div className='flex flex-row'>
+        <img src={img} alt="profile" className='w-[48px] h-[48px] rounded-full'/>
+        <div className='flex flex-col ml-4'>
+          <h4 className='font-poppins font-semibold text-[20px] text-white leading-[32px]'>{name}</h4>
+          <p className='font-poppins font-normal text-[16px] text-dimWhite leading-[24px]'>{title}</p>
         </div>
       </div>
     </div>
@@ -22,12 +22,14 @@ const TestimonialCard = ({content, title, name, img}) => {
 const Testimonial = () => {
   return (
     <section className='flex flex-1 flex-col font-poppins sm:py-16 py-6'>
+      <div className='absolute z-[0] w-[60%] h-[60%] right-[1%] rounded-full blue__gradient' />
+
       <div className='flex md:flex-row flex-col items-center sm:mb-0 mb-10'>
         <h1 className={style.heading2}>What people are <br />saying about us</h1>
         <p className={style.paragraph}>Everything you need to accept card payments and grow your business anywhere on the planet.</p>
       </div>
     
-      <div className='flex md:flex-row flex-col justify-center items-center md:space-x-10'>
+      <div className='flex flex-wrap sm:justify-start justify-center w-full feedback-container relative z-[1]'>
         {feedback.map( (feedback, index) => (
           <TestimonialCard key={feedback.id} {...feedback} index={index} />
         ))}
